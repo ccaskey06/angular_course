47a9fbfc2ecbe9c0d040c62b7b8407fdf6161d50
Corey Caskey <coreyrcaskey@gmail.com>
2021-06-25 18:41:41 -0400

    Recipe App: ch12 Changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  activeFeature: string;
-
-  constructor() {
-    this.activeFeature = 'recipes';
-  }
-
-  onSelected(feature: string) {
-    this.activeFeature = feature;
-    this.featureSelected.emit(feature);
-  }
-}
+export class HeaderComponent { }
